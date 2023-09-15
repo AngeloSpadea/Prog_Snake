e8ce749df671363a6d2fe3b4363668afafe95045
MartinaBertazzoni <martina.bertazzoni.bmh@gmail.com>
2023-09-15 10:25:37 +0200

    Implementato due test per converti_mosse
    E' stato implementato un test per le mosse verticali/orizzontali ed uno per le mosse in direzioni oblique.

--- test_01.py
+++ test_01.py
@@ -1,5 +1,5 @@
 import unittest
-from funzioni_gioco import calcola_mossa
+from funzioni_gioco import calcola_mossa, converti_mosse
 
 class test(unittest.TestCase):
     def test_verifico_calcola_mossa(self):
@@ -9,3 +9,11 @@ class test(unittest.TestCase):
     def test_verifico_calcola_mossa_con_uscita_dal_bordo(self):
         result = calcola_mossa([[3,5]], [1,1], 4, 6)
         self.assertEqual(result, [0, 0])
+
+    def test_corverti_mosse(self):
+        result = converti_mosse(['N'])
+        self.assertEqual(result, [[-1, 0]])
+    
+    def test_corverti_mosse_oblique(self):
+        result = converti_mosse(['NW'])
+        self.assertEqual(result, [[-1, -1]])
