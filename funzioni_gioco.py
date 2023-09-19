e766d3df961192da0e5c314c15bd7bae38cf7304
bigbigbig0 <angelospadea@hotmail.it>
2023-09-20 01:18:45 +0200

    Colorata l'immagine in maniera corretta

--- funzioni_gioco.py
+++ funzioni_gioco.py
@@ -24,14 +24,14 @@ def converti_mosse(mosse):
 
     """
     direzioni = {
-        'N': [0, -1],
-        'S': [0, 1],
-        'E': [1, 0],
-        'W': [-1, 0],
-        'NE': [1, -1],
+        'N': [-1, 0],
+        'S': [1, 0],
+        'E': [0, 1],
+        'W': [0, -1],
+        'NE': [-1, 1],
         'NW': [-1, -1],
         'SE': [1, 1],
-        'SW': [-1, 1]
+        'SW': [1, -1]
     }
     mosse_senza_spazi = mosse.split() 
     mosse_convertite = [direzioni[m] for m in mosse_senza_spazi]
--- gestione_input.py
+++ gestione_input.py
@@ -60,15 +60,15 @@ def restituisco_dati(corpo, scia_serpente, food, blocks, righe, colonne, final_f
     black_image = Image.new("RGB", (colonne, righe), (0, 0, 0))
     
     for x, y in scia_serpente:
-        black_image.putpixel((x, y), (128, 128, 12))
+        black_image.putpixel((y, x), (128, 128, 12))
     
     for x, y in corpo:
-        black_image.putpixel((x, y), (0, 255, 0))          
+        black_image.putpixel((y, x), (0, 255, 0))          
     
     for x, y in food:
-        black_image.putpixel((x, y), (255, 128, 0))
+        black_image.putpixel((y, x), (255, 128, 0))
     
     for x, y in blocks:
-        black_image.putpixel((x, y), (255, 0, 0))
+        black_image.putpixel((y, x), (255, 0, 0))
     
     black_image.save("output/"+final_field)
