150e12c19da790e42658a878619d6b5492852334
antoniospadea <coga.antonio@hotmail.it>
2023-09-24 23:41:56 +0200

    Correzione errore serpente spezzato sulla funzione mangia

--- funzioni_gioco.py
+++ funzioni_gioco.py
@@ -144,7 +144,7 @@ def mangia(corpo, scia_serpente, posizione_nuova, food):
     coda = corpo[-1]
     scia_serpente.insert(0,coda)
     corpo.remove(coda)
-    corpo = [posizione_nuova]+corpo+[posizione_nuova]
+    corpo = [posizione_nuova]+corpo+[coda]
     print(f"il corpo è:{corpo} e la scia è: {scia_serpente}")
     return corpo, scia_serpente
 
